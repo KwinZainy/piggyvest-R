d9a3c745ddf856e60dee86f5bf9b4208976c46b3
KwinZainy <gbadamosiblessing651@gmail.com>
2025-06-20 12:23:35 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Main from './components/Main/Main'
 import Investment from './components/Investment/Investment'
 import Video from './components/Video/Video'
 import About from './components/About/About'
+import Footer from './components/Footer/Footer'
 function App() {
  return(
     <>
@@ -16,6 +17,7 @@ function App() {
   <Investment />
   <Video />
   <About />
+  <Footer />
   </>
  )
 }
