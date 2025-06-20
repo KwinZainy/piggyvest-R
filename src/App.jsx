de145b5b13098af9e385b1188708a26542f1cfe9
KwinZainy <gbadamosiblessing651@gmail.com>
2025-06-20 10:56:41 +0100

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Section from './components/Section/Section'
 function App() {
  return(
     <>
   <Header />
   <Hero />
+  <Section />
   </>
  )
 }
